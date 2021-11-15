c7ce113a75b173de3672258709350488ce65ce31
aldo-castellano <92597205+aldo-castellano@users.noreply.github.com>
2021-11-15 13:40:57 +0100

    add js comments and icon in browser tab. (final version)

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict"
-let computer = false
-const gameSpace = document.querySelectorAll('.game-space');
+let computer = false;
+const gameSpace = document.querySelectorAll(".game-space");
 let divWinner = document.querySelector(".winner");
 let turn = 1;
 let x = [];
 let o = [];
 
-
+/* add css class with display none to the player number selection page and remove it from the game board. select the number of players
+*@param event{object}
+*/
 
 let player = event => {
     const start = document.querySelector(".start");
@@ -20,38 +22,29 @@ let player = event => {
     event.preventDefault();
 }
 
+/* print pacman or ghost elements in DOM
+*@param event{object}, valuePrint{string}
+*/
 
 let print = (event, valuePrint) => {
     event.classList.add(`${valuePrint}`);
-    if (valuePrint.includes('x')) {
-        let pacMan = document.createElement('div')
-        pacMan.classList.add('pac-man');
-        event.appendChild(pacMan)
-        pacMan.innerHTML = `<audio src="./audio/pacman_chomp.aac" autoplay=true ></audio>`
-    } else if (valuePrint.includes('o')) {
-        event.innerHTML = '<div class="ghost"><div class="eyes "></div><div class="pet"></div></div><audio src="./audio/pacman_ghost.aac" autoplay=true >'
+    if (valuePrint.includes("x")) {
+        let pacMan = document.createElement("div");
+        pacMan.classList.add("pac-man");
+        event.appendChild(pacMan);
+        pacMan.innerHTML = `<audio src="./audio/pacman_chomp.aac" autoplay=true ></audio>`;
+
+    } else if (valuePrint.includes("o")) {
+        event.innerHTML = `<div class="ghost"><div class="eyes "></div><div class="pet"></div></div><audio src="./audio/pacman_ghost.aac" autoplay=true >`;
     }
 
 }
 
 
-let reset = () => {
-    turn = 1;
-    x = [];
-    o = [];
-    gameSpace.forEach(e => {
-        e.classList.remove('x')
-        e.classList.remove('o')
-        e.innerHTML = '';
-    })
-    divWinner.classList.add('none');
-    let deleteWinner = document.querySelector('.showing-winner')
-    divWinner.removeChild(deleteWinner)
+/* create random value on computer turn to simulate opponent
+@return if the box is empty
+*/
 
-
-
-
-}
 
 let computerTurn = () => {
 
@@ -67,13 +60,15 @@ let computerTurn = () => {
             o.push(parseInt(randomBoxValue.attributes.id.value));
             winner(o) ? printWinner("o", "WINNER") : null;
 
-            return
+            return;
         }
     }
 
 
 }
-
+/* click event function that evaluates whose turn it is, if the player won
+*@param event{object}
+*/
 const gameLogic = (event) => {
 
     let testOX = /[o,x]/.test(event.target.attributes.class.value);
@@ -100,43 +95,65 @@ const gameLogic = (event) => {
     }
 }
 
-let printWinner = (winner, nobadyWin) => {
-    let showingWinner = document.createElement('div');
+/* function of an event that resets the entire game board*/
+let reset = () => {
+    turn = 1;
+    x = [];
+    o = [];
+    gameSpace.forEach(e => {
+        e.classList.remove("x");
+        e.classList.remove("o");
+        e.innerHTML = "";
+    })
+    divWinner.classList.add("none");
+    let deleteWinner = document.querySelector(".showing-winner");
+    divWinner.removeChild(deleteWinner);
+
+}
+
+/* print the winner"s screen
+*@param winner{string}, statement{string}
+*/
+let printWinner = (winner, statement) => {
+    let showingWinner = document.createElement("div");
     turn = null;
-    divWinner.classList.remove('none');
+    divWinner.classList.remove("none");
 
-    showingWinner.classList.add('showing-winner')
-    showingWinner.classList.add(`${winner}`)
-    showingWinner.innerHTML = `${nobadyWin}`;
+    showingWinner.classList.add("showing-winner");
+    showingWinner.classList.add(`${winner}`);
+    showingWinner.innerHTML = `${statement}`;
 
 
-    if (winner === 'x') {
+    if (winner === "x") {
 
-        showingWinner.innerHTML = `<p>${nobadyWin}</p> <div class="pac-man no-index"><audio src="./audio/pacman_eatghost.aac" autoplay=true ></div>`
-    } else if (winner === 'o') {
-        showingWinner.innerHTML = `<p>${nobadyWin} <div class="ghost no-index"><div class="eyes "></div><div class="pet"></div></div><audio src="./audio/pacman_death.aac" autoplay=true >`
+        showingWinner.innerHTML = `<p>${statement}</p> <div class="pac-man no-index"><audio src="./audio/pacman_eatghost.aac" autoplay=true ></div>`;
+    } else if (winner === "o") {
+        showingWinner.innerHTML = `<p>${statement} <div class="ghost no-index"><div class="eyes "></div><div class="pet"></div></div><audio src="./audio/pacman_death.aac" autoplay=true >`;
     }
 
     divWinner.appendChild(showingWinner);
 }
-
+/* evaluate whether or not there is a winner
+*@param arr{array of numbers}
+@return true/false
+*/
 const winner = (arr) => {
 
     if (arr.includes(0) && arr.includes(1) && arr.includes(2) || arr.includes(3) && arr.includes(4) && arr.includes(5) || arr.includes(6) && arr.includes(7) && arr.includes(8)) {
 
-        return true
+        return true;
     } else if (arr.includes(0) && arr.includes(3) && arr.includes(6) || arr.includes(1) && arr.includes(4) && arr.includes(7) || arr.includes(2) && arr.includes(5) && arr.includes(8)) {
 
-        return true
+        return true;
     } else if (arr.includes(0) && arr.includes(4) && arr.includes(8) || arr.includes(2) && arr.includes(4) && arr.includes(6)) {
 
-        return true
+        return true;
     } else if ((x.length + o.length) >= gameSpace.length) {
 
-        printWinner("no-winner", "NADIE GANO")
+        printWinner("no-winner", "NADIE GANO");
     }
 
-    return false
+    return false;
 
 }
 
